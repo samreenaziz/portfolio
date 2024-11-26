7af13959c7cf9481c237fa9e62c1c80746c8ecf6
samreenaziz <samreen.aaziz@gmail.com>
2024-11-25 19:55:50 -0500

    Header completed 90%, will need to return to this after adding in more content to align header section bolding on scroll/in-view state.

--- lib/data.ts
+++ lib/data.ts
@@ -2,9 +2,12 @@ import React from "react";
 import { CgWorkAlt } from "react-icons/cg";
 import { FaReact } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
-import corpcommentImg from "@/public/corpcomment.png";
-import rmtdevImg from "@/public/rmtdev.png";
-import wordanalyticsImg from "@/public/wordanalytics.png";
+import cuppaImg from "@/public/cuppa.png";
+import topgunImg from "@/public/topgun.png";
+import travelSiteImg from "@/public/travelsite.png";
+import bandSiteImg from "@/public/bandsite1.png";
+import coffeeShopImg from "@/public/coffeeshop.png";
+
 
 export const links = [
   {
@@ -62,25 +65,25 @@ export const experiencesData = [
 
 export const projectsData = [
   {
-    title: "CorpComment",
+    title: "Cuppa",
     description:
-      "I worked as a full-stack developer on this startup project for 2 years. Users can give public feedback to companies.",
-    tags: ["React", "Next.js", "MongoDB", "Tailwind", "Prisma"],
-    imageUrl: corpcommentImg,
+      "This is an MVP of a postpartum peer-support application that I am building as part of my efforts to support women's digital health resources. I am the full-stack developer for this project.",
+    tags: ["React", "Express", "Node.js", "MySQL", "CSS/Sass", "Google Cloud Console", "Google API", "Heroku CLI", "JawsDB"],
+    imageUrl: cuppaImg,
   },
   {
-    title: "rmtDev",
+    title: "Hunter Safety Education Booking Site",
     description:
       "Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.",
-    tags: ["React", "TypeScript", "Next.js", "Tailwind", "Redux"],
-    imageUrl: rmtdevImg,
+    tags: ["React", "Express.js", "Node.js", "MySQL", "CSS/Sass", "Framer Motion"],
+    imageUrl: topgunImg,
   },
   {
-    title: "Word Analytics",
+    title: "Promotional Band Site",
     description:
       "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
-    tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
-    imageUrl: wordanalyticsImg,
+    tags: ["React", "Express.js", "Node.js", "MySQL", "CSS/Sass"],
+    imageUrl: bandSiteImg,
   },
 ] as const;
 
