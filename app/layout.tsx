29bf88862c9513ef1644d8f649926cd1abf5fb6b
samreenaziz <samreen.aaziz@gmail.com>
2024-12-27 12:55:52 -0500

    trying again without public on the image of interest

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,6 @@ import ActiveSectionContextProvider from '@/context/active-section-context'
 import { ToastContainer } from 'react-toastify';
 import Footer from '@/components/footer';
 import Head from 'next/head'
-import profileImg from "@/public/profile-image.jpg"
 
 
 const inter = Inter({ subsets: ['latin'] })
@@ -13,7 +12,7 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata = {
   title: 'Samreen Aziz | Professional Portfolio',
   description: 'Samreen is a data scientist, developer, and tech for social good enthusiast with over 5 years of experience in the tech industry.',
-  image: 'https://samreenazizportfolio.vercel.app' + profileImg.src,
+  image: 'https://samreenazizportfolio.vercel.app' + "/profile-image.jpg",
 }
 
 export default function RootLayout({
